7dfc97ca21c4c0d1f669cfdcd9cea9b5e82666bb
mileycirusprograming <tar.inem@googlemail.com>
2019-04-29 14:09:47 +0200

    blacklisted lists all found words

--- script.py
+++ script.py
@@ -4,20 +4,18 @@ import codecs
 
 url = "https://www.ebay.de/itm/Original-Audi-19-Zoll-Felgen-A4-S4-B9-Alufelgen-Sommerreifen-Neu-Sommerrader/173837096716?_trkparms=aid%3D111001%26algo%3DREC.SEED%26ao%3D1%26asc%3D20180816085401%26meid%3Dea18f30fdbb747cab5190f43fe9b28ba%26pid%3D100970%26rk%3D2%26rkt%3D2%26sd%3D290870810757%26itm%3D173837096716&_trksid=p2481888.c100970.m5481&_trkparms=pageci%3Ad401779d-4a6c-11e9-94c6-74dbd180f0d4%7Cparentrq%3A97020f691690ad78ab4a7810fffe176a%7Ciid%3A1"
 
-NOT_FOUND = "/wordnotfound"
-
 def check_blacklist(blacklist_path, text):
     #open file for checking
     file_handler = codecs.open(blacklist_path, 'r', 'iso-8859-15')
     lines = file_handler.read().splitlines()
 
+    found_words = []
     #check for every blacklisted word
     for line in lines:
         if text.find(line) > -1:
             #blacklisted word found
-            return line
-    #no blacklisted word found
-    return NOT_FOUND
+            found_words.append(line)
+    return found_words
 
 def check_mandatory(mandatorylist_path, text):
     #open file for checking
